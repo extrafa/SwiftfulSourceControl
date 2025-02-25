8a518fd0aac172ff541e4b6d7831a392399768f5
Ross <rostislavpredein@gmail.com>
2025-02-25 00:17:55 -0300

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Силиконовый Бургер on 25.02.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
