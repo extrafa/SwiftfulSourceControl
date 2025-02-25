b54c3ae3ad247bb660c576f2eb04a3e7b86772e1
Ross <rostislavpredein@gmail.com>
2025-02-24 22:49:09 -0300

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Силиконовый Бургер on 24.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
