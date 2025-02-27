21c5805258e2f3f6bece934b02f5ae5797d36a6e
Ross <rostislavpredein@gmail.com>
2025-02-27 00:23:48 -0300

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by Силиконовый Бургер on 24.02.2025.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ */
+
+
 import SwiftUI
 
 struct ContentView: View {
