36ff9fac895acca4cbecaa102ecaf9042276a3a6
Ross <rostislavpredein@gmail.com>
2025-03-04 00:03:48 -0300

    [Clean] Updates to documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -6,6 +6,13 @@
 //
 
 /*
+ Clone = Copying the repo locally
+ Commit = Save ("checkpoint") on our current branch
+ Stage = Prepare changes for commit
+ Stash = Save changes for later
+ Push = Send local commits to remote repo
+ Pull = Fetch remote commits to remote repo
+ 
  COMMIT MESSAGES
  
  NEW FEATURE:
