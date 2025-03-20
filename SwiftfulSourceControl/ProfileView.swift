d23ad96a7e62c6cd9bf1d37a9b1cf840f326d0f9
Ross <rostislavpredein@gmail.com>
2025-03-20 19:53:40 -0300

    Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Ross on 20.03.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Ross!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
